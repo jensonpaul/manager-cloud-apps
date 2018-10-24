a4f124ce607971ebf7a96bf1060ce124a7d6220b
raminarmanfar <ramin.armanfar@msg.group>
2018-10-24 12:25:54 +0200

    documentation improved

--- manager/up.sh
+++ manager/up.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
 if [ -z ${ENV+x} ]; then
 	echo "+ Error: Source (dev|prod)/definations.sh first."
 	exit 1
 else
 	echo "+ Using [${ENV}] configuration."
-
+	
 	if [ "$ENV" == "Production" ]; then
 		docker-compose -f docker-compose.yml -f ./prod/docker-compose.yml up -d
 	else
